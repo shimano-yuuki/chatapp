d2bd5adf652b0370d3f92b6e66eae0f2792d27c8
simanoyuki <yuukireirei5@gmail.com>
2022-11-18 16:02:02 +0900

    chatapp

---
+++ lib/main.dart
@@ -0,0 +1,166 @@
+import 'package:chatapp/post.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  // main 関数でも async が使えます
+  WidgetsFlutterBinding.ensureInitialized(); // runApp 前に何かを実行したいときはこれが必要です。
+  await Firebase.initializeApp(
+    // これが Firebase の初期化処理です。
+    options: DefaultFirebaseOptions.android,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      home: const SignInPage(),
+    );
+  }
+}
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('チャット'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot<Post>>(
+              // stream プロパティに snapshots() を与えると、コレクションの中のドキュメントをリアルタイムで監視することができます。
+              stream: postsReference.orderBy('createdAt').snapshots(),
+              // ここで受け取っている snapshot に stream で流れてきたデータが入っています。
+              builder: (context, snapshot) {
+                // docs には Collection に保存されたすべてのドキュメントが入ります。
+                // 取得までには時間がかかるのではじめは null が入っています。
+                // null の場合は空配列が代入されるようにしています。
+                final docs = snapshot.data?.docs ?? [];
+                return ListView.builder(
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) {
+                    // data() に Post インスタンスが入っています。
+                    // これは withConverter を使ったことにより得られる恩恵です。
+                    // 何もしなければこのデータ型は Map になります。
+                    final post = docs[index].data();
+                    return Text(post.text);
+                  },
+                );
+              },
+            ),
+          ),
+          TextFormField(
+            onFieldSubmitted: (text) {
+              // まずは user という変数にログイン中のユーザーデータを格納します
+              final user = FirebaseAuth.instance.currentUser!;
+
+              final posterId = user.uid; // ログイン中のユーザーのIDがとれます
+              final posterName = user.displayName!; // Googleアカウントの名前がとれます
+              final posterImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
+
+              // 先ほど作った postsReference からランダムなIDのドキュメントリファレンスを作成します
+              // doc の引数を空にするとランダムなIDが採番されます
+              final newDocumentReference = postsReference.doc();
+
+              final newPost = Post(
+                text: text,
+                createdAt: Timestamp.now(), // 投稿日時は現在とします
+                posterName: posterName,
+                posterImageUrl: posterImageUrl,
+                posterId: posterId,
+                reference: newDocumentReference,
+              );
+
+              // 先ほど作った newDocumentReference のset関数を実行するとそのドキュメントにデータが保存されます。
+              // 引数として Post インスタンスを渡します。
+              // 通常は Map しか受け付けませんが、withConverter を使用したことにより Post インスタンスを受け取れるようになります。
+              newDocumentReference.set(newPost);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SignInPageState extends State<SignInPage> {
+  Future<void> signInWithGoogle() async {
+    final googleUser =
+        await GoogleSignIn(scopes: ['profile', 'email']).signIn();
+
+    final googleAuth = await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GoogleSignIn'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('GoogleSignIn'),
+          onPressed: () async {
+            await signInWithGoogle();
+            print(FirebaseAuth.instance.currentUser?.displayName);
+
+            // ログインに成功したら ChatPage に遷移します。
+            // 前のページに戻らせないようにするにはpushAndRemoveUntilを使います。
+            if (mounted) {
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) {
+                  return const ChatPage();
+                }),
+                (route) => false,
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+final postsReference =
+    FirebaseFirestore.instance.collection('posts').withConverter<Post>(
+  // <> ここに変換したい型名をいれます。今回は Post です。
+  fromFirestore: ((snapshot, _) {
+    // 第二引数は使わないのでその場合は _ で不使用であることを分かりやすくしています。
+    return Post.fromFirestore(snapshot); // 先ほど定期着した fromFirestore がここで活躍します。
+  }),
+  toFirestore: ((value, _) {
+    return value.toMap(); // 先ほど適宜した toMap がここで活躍します。
+  }),
+);
